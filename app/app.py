3522f37aaeb6fc93153f18c287a8a368388ce288
wproffi2 <wnprfftt@gmail.com>
2019-05-10 17:02:38 -0400

    added back buttons/CSS, fixed some html

--- app/app.py
+++ app/app.py
@@ -29,7 +29,6 @@ from flask_jwt_extended import (
 )
 
 
-
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
